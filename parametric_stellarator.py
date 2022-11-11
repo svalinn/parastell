a30f0dbe772cda4ab3c3c6e54924afca86360af0
Connor Moreno <camoreno@Denali.ep.wisc.edu>
2022-11-11 12:52:15 -0600

    Make suggested changes

--- parametric_stellarator.py
+++ parametric_stellarator.py
@@ -5,10 +5,11 @@ import read_vmec
 from paramak.utils import export_solids_to_dagmc_h5m
 
 
-def offset_point(zeta, theta, offset):
+def offset_point(vmec, zeta, theta, offset):
     """Stellarator offset surface root-finding problem.
 
     Arguments:
+        vmec (object): plasma equilibrium object.
         zeta (float): toroidal angle being solved for (rad).
         theta (float): poloidal angle of interest (rad).
         offset (float): total offset of layer from plamsa (m).
@@ -21,9 +22,12 @@ def offset_point(zeta, theta, offset):
     # angles of interest
     r = np.array(vmec.vmec2xyz(1.0, theta, zeta))
     
+    # Define small number
+    eta = 0.000001
+
     # Vary poloidal and toroidal angles by small amount
-    r_phi = np.array(vmec.vmec2xyz(1.0, theta, zeta + 0.000001))
-    r_theta = np.array(vmec.vmec2xyz(1.0, theta + 0.000001, zeta))
+    r_phi = np.array(vmec.vmec2xyz(1.0, theta, zeta + eta))
+    r_theta = np.array(vmec.vmec2xyz(1.0, theta + eta, zeta))
     
     # Take difference from plasma edge point
     delta_phi = r_phi - r
@@ -40,11 +44,12 @@ def offset_point(zeta, theta, offset):
     return pt
 
 
-def root_problem(zeta, theta, phi, offset):
+def root_problem(zeta, vmec, theta, phi, offset):
     """Stellarator offset surface root-finding problem.
 
     Arguments:
         zeta (float): toroidal angle being solved for (rad).
+        vmec (object): plasma equilibrium object.
         theta (float): poloidal angle of interest (rad).
         phi (float): toroidal angle of interest (rad).
         offset (float): total offset of layer from plamsa (m).
@@ -54,7 +59,7 @@ def root_problem(zeta, theta, phi, offset):
             angle of interest (root-finding problem definition).
     """
     # Define offset surface
-    x, y, z = offset_point(zeta, theta, offset)
+    x, y, z = offset_point(vmec, zeta, theta, offset)
 
     offset_phi = np.arctan2(y, x)
 
@@ -66,10 +71,11 @@ def root_problem(zeta, theta, phi, offset):
     return diff
 
 
-def offset_surface(theta, phi, offset):
+def offset_surface(vmec, theta, phi, offset):
     """Computes offset surface point.
 
     Arguments:
+        vmec (object): plasma equilibrium object.
         theta (float): poloidal angle of interest (rad).
         phi (float): toroidal angle of interest (rad).
         offset (float): total offset of layer from plamsa (m).
@@ -80,58 +86,56 @@ def offset_surface(theta, phi, offset):
     # Root-solve for the toroidal angle at which the plasma
     # surface normal points to the toroidal angle of interest
     zeta = root_scalar(
-        root_problem, args = (theta, phi, offset), x0 = phi, method = 'bisect',
-        bracket = [phi - np.pi/4, phi + np.pi/4])
+        root_problem, args = (vmec, theta, phi, offset), x0 = phi,
+        method = 'bisect', bracket = [phi - np.pi/4, phi + np.pi/4])
     zeta = zeta.root
 
     # Compute offset surface point
-    r = offset_point(zeta, theta, offset)
+    r = offset_point(vmec, zeta, theta, offset)
 
     return r
 
 
-def stellarator_torus(offset, cutter, num_periods):
+def stellarator_torus(vmec, offset, cutter, num_periods, num_phi, num_theta):
     """Creates a stellarator helical torus as a CadQuery object.
     
     Arguments:
+        vmec (object): plasma equilibrium object.
         offset (float): total offset of layer from plamsa (cm).
         cutter (object): CadQuery solid object used to cut period of
             stellarator torus.
         num_periods (int): number of stellarator periods to build in model.
+        num_phi (int): number of phi geometric cross-sections to make.
+        num_theta (int): number of points defining the geometric cross-section.
     
     Returns:
         torus (object): stellarator torus CadQuery solid object.
         cutter(object): updated cutting volume CadQuery solid object.
     """
-    # Define the number of phi geometric cross-sections to make
-    num_phi = 60
-    # Define the number of points defining the geometric cross-section
-    num_theta = 100
-
     # Define toroidal (phi) and poloidal (theta) arrays
-    phi = np.linspace(0, np.pi/2, num = num_phi + 1)
-    theta = np.linspace(0, 2*np.pi, num = num_theta + 1)
+    phi_list = np.linspace(0, np.pi/2, num = num_phi + 1)
+    theta_list = np.linspace(0, 2*np.pi, num = num_theta + 1)[:-1]
 
     # Initialize construction
     period = cq.Workplane("XY")
 
     # Generate poloidal profiles
-    for i in phi:
+    for phi in phi_list:
         
         # Initialize points in poloidal profile
         pts = []
         
         # Compute array of points along poloidal profile
-        for j in theta[:-1]:
+        for theta in theta_list:
             
             # Conditionally offset poloidal profile
             if offset == 0:
                 # Compute plasma edge point
-                x, y, z = vmec.vmec2xyz(1.0, j, i)
+                x, y, z = vmec.vmec2xyz(1.0, theta, phi)
             
             else:
                 # Compute offset surface point
-                x, y, z = offset_surface(j, i, offset)
+                x, y, z = offset_surface(vmec, theta, phi, offset)
             
             # Convert from m to cm
             pt = (x*100, y*100, z*100)
@@ -156,30 +160,27 @@ def stellarator_torus(offset, cutter, num_periods):
     # Update cutting volume
     cutter = period
 
-    # Define torus as conditionally cut period
+    # Initialize torus with conditionally cut period
     torus = period_cut
 
-    # Conditionally generate additional periods
-    if num_periods > 1:
+    # Define initial angles defining each period needed
+    initial_angles = np.linspace(
+        90.0, 90.0*(num_periods - 1), num = num_periods - 1
+        )
 
-        # Define initial angles defining each period needed
-        initial_angles = np.linspace(
-            90.0, 90.0*(num_periods - 1), num = num_periods - 1
-            )
-
-        # Generate additional profiles
-        for angle in initial_angles:
-            period = period_cut.rotate((0, 0, 0), (0, 0, 1), angle)
-            torus = torus.union(period)
+    # Generate additional profiles
+    for angle in initial_angles:
+        period = period_cut.rotate((0, 0, 0), (0, 0, 1), angle)
+        torus = torus.union(period)
 
     return torus, cutter
 
 
 def parametric_stellarator(
-    plas_eq, radial_build, num_periods, exclude = [], step_export = True,
-    h5m_export = True, h5m_tags = None, include_graveyard = False, 
-    min_mesh_size = 5.0, max_mesh_size = 20.0, volume_atol = 0.00001,
-    center_atol = 0.00001, bounding_box_atol = 0.00001):
+    plas_eq, radial_build, num_periods, num_phi = 60, num_theta = 100,
+    exclude = [], step_export = True, h5m_export = True, h5m_tags = None,
+    include_graveyard = False, min_mesh_size = 5.0, max_mesh_size = 20.0,
+    volume_atol = 0.00001, center_atol = 0.00001, bounding_box_atol = 0.00001):
     """Generates CadQuery workplane objects for components of a
     parametrically-defined stellarator, based on user-supplied plasma
     equilibrium VMEC data and a user-defined radial build. Each component is
@@ -193,6 +194,10 @@ def parametric_stellarator(
             form {'component': thickness (cm)}. Concentric layers will be built
             in the order given.
         num_periods (int): number of stellarator periods to build in model.
+        num_phi (int): number of phi geometric cross-sections to make (defaults
+            to 60).
+        num_theta (int): number of points defining the geometric cross-section
+            (defaults to 100).
         exclude (list of str): names of components not to export.
         step_export (bool): export component .step fiiles
         h5m_export (bool): export DAGMC-compatible neutronics .h5m file using
@@ -212,7 +217,6 @@ def parametric_stellarator(
             when matching parts in intermediate BREP file with CadQuery parts.
     """
     # Load plasma equilibrium data
-    global vmec
     vmec = read_vmec.vmec_data(plas_eq)
 
     # Initialize offset value
@@ -226,7 +230,8 @@ def parametric_stellarator(
     cutter = None
 
     # Generate plasma STEP file
-    plasma, cutter = stellarator_torus(offset, cutter, num_periods)
+    plasma, cutter = stellarator_torus(vmec, offset, cutter, num_periods,
+        num_phi, num_theta)
 
     # Optionally store and export plasma
     if 'plasma' not in exclude:
@@ -249,7 +254,8 @@ def parametric_stellarator(
         offset += thickness/100
 
         # Generate component
-        torus, cutter = stellarator_torus(offset, cutter, num_periods)
+        torus, cutter = stellarator_torus(vmec, offset, cutter, num_periods,
+            num_phi, num_theta)
 
         # Store solid and name tag, export component
         if name not in exclude:
