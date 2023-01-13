8882ba1cad50b22e6a978f0ed9b61341c731acbb
Connor Moreno <camoreno@Denali.ep.wisc.edu>
2023-01-12 19:08:11 -0600

    Restructure code and improve documentation

--- ExampleScript.py
+++ ExampleScript.py
@@ -1,17 +1,27 @@
 import parametric_stellarator
 
 
+# Define plasma equilibrium VMEC file
 plas_eq = 'wout_daz.nc'
+# Define radial build
 radial_build = {
-    'sol': 10,
-    'first_wall': 5,
-    'blanket': 50,
-    'back_wall': 5,
-    'shield': 30,
-    'coolant_manifolds': 30,
-    'gap': 20,
-    'vacuum_vessel': 30
-    }
+'sol': {'thickness': 10, 'h5m_tag': 'Vacuum'},
+'first_wall': {'thickness': 5},
+'blanket': {'thickness': 50},
+'back_wall': {'thickness': 5},
+'shield': {'thickness': 30},
+'coolant_manifolds': {'thickness': 30},
+'gap': {'thickness': 20, 'h5m_tag': 'Vacuum'},
+'vacuum_vessel': {'thickness': 30}
+}
+# Define number of periods in stellarator plasma
 num_periods = 4
+# Define number of periods to generate
+gen_periods = 4
 
-parametric_stellarator.parametric_stellarator(plas_eq, radial_build, num_periods)
+# Create stellarator
+parametric_stellarator.parametric_stellarator(
+    plas_eq, num_periods, radial_build, gen_periods,
+    exclude = ['plasma', 'sol'], h5m_export = 'cubit', plas_h5m_tag = 'Vacuum',
+    facet_tol = 1e-2, len_tol = 1
+    )
