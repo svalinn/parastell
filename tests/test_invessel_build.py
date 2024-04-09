f72691ce7be591e8ecc869fe5fd15498f54e8404
Connor Moreno <camoreno@Denali.ep.wisc.edu>
2024-04-09 17:45:21 -0500

    Incorporate pytest fixtures

--- tests/test_invessel_build.py
+++ tests/test_invessel_build.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import numpy as np
+import pytest
 
 # import this before read_vmec to deal with conflicting 
 # dependencies correctly
@@ -9,73 +10,107 @@ import parastell.invessel_build as ivb
 import read_vmec
 
 
-if Path('plasma.step').exists():
-    Path.unlink('plasma.step')
-if Path('sol.step').exists():
-    Path.unlink('sol.step')
-if Path('component.step').exists():
-    Path.unlink('component.step')
-if Path('dagmc.h5m').exists():
-    Path.unlink('dagmc.h5m')
+def remove_files():
+    
+    if Path('plasma.step').exists():
+        Path.unlink('plasma.step')
+    if Path('sol.step').exists():
+        Path.unlink('sol.step')
+    if Path('component.step').exists():
+        Path.unlink('component.step')
+    if Path('dagmc.h5m').exists():
+        Path.unlink('dagmc.h5m')
 
-vmec_file = Path('files_for_tests') / 'wout_vmec.nc'
-vmec = read_vmec.vmec_data(vmec_file)
 
-toroidal_angles_exp = [0.0, 5.0, 10.0, 15.0]
-poloidal_angles_exp = [0.0, 120.0, 240.0, 360.0]
-radial_build = {
-    'component': {
-        'thickness_matrix': np.ones(
-            (len(toroidal_angles_exp), len(poloidal_angles_exp))
-        )*10
+@pytest.fixture
+def invessel_build():
+    
+    vmec_file = Path('files_for_tests') / 'wout_vmec.nc'
+    vmec = read_vmec.vmec_data(vmec_file)
+
+    toroidal_angles = [0.0, 5.0, 10.0, 15.0]
+    poloidal_angles = [0.0, 120.0, 240.0, 360.0]
+    radial_build = {
+        'component': {
+            'thickness_matrix': np.ones(
+                (len(toroidal_angles), len(poloidal_angles))
+            )*10
+        }
     }
-}
-wall_s_exp = 1.08
-repeat_exp = 0
-num_ribs_exp = 11
-num_rib_pts_exp = 67
-scale_exp = 100
-plasma_mat_tag_exp = 'Vacuum'
-sol_mat_tag_exp = 'Vacuum'
+    wall_s = 1.08
+    repeat = 0
+    num_ribs = 11
+    num_rib_pts = 67
+    scale = 100
+    plasma_mat_tag = 'Vacuum'
+    sol_mat_tag = 'Vacuum'
 
-invessel_build = ivb.InVesselBuild(
-    vmec, toroidal_angles_exp, poloidal_angles_exp, radial_build, wall_s_exp,
-    repeat=repeat_exp, num_ribs=num_ribs_exp, num_rib_pts=num_rib_pts_exp,
-    scale=scale_exp, plasma_mat_tag=plasma_mat_tag_exp,
-    sol_mat_tag=sol_mat_tag_exp
-)
+    ivb_obj = ivb.InVesselBuild(
+        vmec, toroidal_angles, poloidal_angles, radial_build, wall_s,
+        repeat=repeat, num_ribs=num_ribs, num_rib_pts=num_rib_pts, scale=scale,
+        plasma_mat_tag=plasma_mat_tag, sol_mat_tag=sol_mat_tag
+    )
+
+    return ivb_obj
 
 
-def test_ivb_basics():
+def test_ivb_basics(invessel_build):
 
+    toroidal_angles_exp = [0.0, 5.0, 10.0, 15.0]
+    poloidal_angles_exp = [0.0, 120.0, 240.0, 360.0]
+    num_components_exp = 3
+    wall_s_exp = 1.08
+    repeat_exp = 0
+    num_ribs_exp = 11
+    num_rib_pts_exp = 67
+    scale_exp = 100
+    plasma_mat_tag_exp = 'Vacuum'
+    sol_mat_tag_exp = 'Vacuum'
+
+    remove_files()
+    
     invessel_build.populate_surfaces()
 
     assert np.allclose(invessel_build.toroidal_angles, toroidal_angles_exp)
     assert np.allclose(invessel_build.poloidal_angles, poloidal_angles_exp)
+    assert len(invessel_build.radial_build.keys()) == num_components_exp
     assert invessel_build.wall_s == wall_s_exp
-    assert len(invessel_build.radial_build.keys()) == 3
     assert (
-        invessel_build.radial_build['plasma']['mat_tag'] == 'Vacuum'
+        invessel_build.radial_build['plasma']['mat_tag'] == plasma_mat_tag_exp
     )
     assert (
-        invessel_build.radial_build['sol']['mat_tag'] == 'Vacuum'
+        invessel_build.radial_build['sol']['mat_tag'] == sol_mat_tag_exp
     )
     assert invessel_build.repeat == repeat_exp
     assert invessel_build.num_ribs == num_ribs_exp
     assert invessel_build.num_rib_pts == num_rib_pts_exp
     assert invessel_build.scale == scale_exp
 
+    remove_files()
 
-def test_ivb_construction():
 
+def test_ivb_construction(invessel_build):
+
+    num_components_exp = 3
+
+    remove_files()
+
+    invessel_build.populate_surfaces()
     invessel_build.calculate_loci()
     invessel_build.generate_components()
 
-    assert len(invessel_build.Components) == 3
+    assert len(invessel_build.Components) == num_components_exp
+
+    remove_files()
 
 
-def test_ivb_exports():
+def test_ivb_exports(invessel_build):
     
+    remove_files()
+
+    invessel_build.populate_surfaces()
+    invessel_build.calculate_loci()
+    invessel_build.generate_components()
     invessel_build.export_step()
     """
     CAD-to-DAGMC export does not currently work; might be an incompatibility
@@ -85,13 +120,6 @@ def test_ivb_exports():
     """
     assert Path('plasma.step').exists() 
     assert Path('sol.step').exists() 
-    assert Path('component.step').exists() 
+    assert Path('component.step').exists()
 
-    Path.unlink('plasma.step')
-    Path.unlink('sol.step')
-    Path.unlink('component.step')
-    Path.unlink('stellarator.log')
-
-test_ivb_basics()
-test_ivb_construction()
-test_ivb_exports()
+    remove_files() 
--- tests/test_source_mesh.py
+++ tests/test_source_mesh.py
@@ -1,12 +1,24 @@
-import parastell.source_mesh as sm
-import read_vmec as rv
-import numpy as np
 from pathlib import Path
 
-vmec_file = Path('files_for_tests') / 'wout_vmec.nc'
+import numpy as np
+import pytest
+import read_vmec as rv
+
+import parastell.source_mesh as sm
 
 
-def test_mesh_basics():
+def remove_files():
+
+    if Path('source_mesh.h5m').exists():
+        Path.unlink('source_mesh.h5m')
+    if Path('stellarator.log').exists():
+        Path.unlink('stellarator.log')
+
+
+@pytest.fixture
+def source_mesh():
+
+    vmec_file = Path('files_for_tests') / 'wout_vmec.nc'
 
     vmec = rv.vmec_data(vmec_file)
 
@@ -16,10 +28,23 @@ def test_mesh_basics():
     tor_ext_exp = 90.0
     scale_exp = 100
 
-    source_mesh = sm.SourceMesh(
+    source_mesh_obj = sm.SourceMesh(
         vmec, num_s_exp, num_theta_exp, num_phi_exp, tor_ext_exp,
         scale=scale_exp
     )
+
+    return source_mesh_obj
+
+
+def test_mesh_basics(source_mesh):
+
+    num_s_exp = 4
+    num_theta_exp = 8
+    num_phi_exp = 4
+    tor_ext_exp = 90.0
+    scale_exp = 100
+
+    remove_files()
 
     assert source_mesh.num_s == num_s_exp
     assert source_mesh.num_theta == num_theta_exp
@@ -27,44 +52,31 @@ def test_mesh_basics():
     assert source_mesh.toroidal_extent == np.deg2rad(tor_ext_exp)
     assert source_mesh.scale == scale_exp
 
+    remove_files()
 
-def test_vertices():
 
-    vmec = rv.vmec_data(vmec_file)
+def test_vertices(source_mesh):
 
-    num_s_exp = 4
-    num_theta_exp = 8
-    num_phi_exp = 4
-    tor_ext_exp = 90.0
-    scale_exp = 100
+    num_s = 4
+    num_theta = 8
+    num_phi = 4
 
-    num_verts_exp = num_phi_exp * ((num_s_exp - 1) * (num_theta_exp - 1) + 1)
+    num_verts_exp = num_phi * ((num_s - 1) * (num_theta - 1) + 1)
+
+    remove_files()
     
-    source_mesh = sm.SourceMesh(
-        vmec, num_s_exp, num_theta_exp, num_phi_exp, tor_ext_exp,
-        scale=scale_exp
-    )
-
     source_mesh.create_vertices()
 
     assert source_mesh.coords.shape == (num_verts_exp, 3)
     assert source_mesh.coords_s.shape == (num_verts_exp,)
     assert len(source_mesh.verts) == num_verts_exp
 
-def test_export():
+    remove_files()
 
-    vmec = rv.vmec_data(vmec_file)
 
-    num_s_exp = 4
-    num_theta_exp = 8
-    num_phi_exp = 4
-    tor_ext_exp = 90.0
-    scale_exp = 100
+def test_export(source_mesh):
 
-    source_mesh = sm.SourceMesh(
-        vmec, num_s_exp, num_theta_exp, num_phi_exp, tor_ext_exp,
-        scale=scale_exp
-    )
+    remove_files()
 
     source_mesh.create_vertices()
     source_mesh.create_mesh()
@@ -72,5 +84,5 @@ def test_export():
 
     assert Path('source_mesh.h5m').exists()
 
-    Path.unlink('source_mesh.h5m')
-    Path.unlink('stellarator.log')
+    remove_files()
+    
