a0d5f20be77b7838b104270a329c0dab65c2e2a1
Edgar <ejpflug@gmail.com>
2024-09-03 14:52:50 -0500

    changes per pr comments

--- parastell/radial_distance_utils.py
+++ parastell/radial_distance_utils.py
@@ -1,7 +1,4 @@
 import numpy as np
-
-# from matplotlib import pyplot as plt
-# from matplotlib.ticker import FormatStrFormatter
 from . import magnet_coils
 from . import invessel_build as ivb
 import pystell.read_vmec as read_vmec
@@ -33,16 +30,13 @@ def get_start_index(filament):
         max_z_index (int): index at which the filament crosses the xy plane
     """
     max_z_index = None
-    max_z_radius = None
-    for index, point in enumerate(filament[0:-1]):
-        next_point = filament[index + 1]
+    max_z_radius = 0
+    for index, (point, next_point) in enumerate(
+        zip(filament[0:-1], filament[1:])
+    ):
         if point[2] / next_point[2] < 0:
             z_radius = calc_z_radius(point)
-            if max_z_radius is None:
-                max_z_index = index
-                max_z_radius = z_radius
-
-            elif max_z_radius < z_radius:
+            if max_z_radius < z_radius:
                 max_z_index = index
                 max_z_radius = z_radius
     return max_z_index
@@ -59,13 +53,11 @@ def sort_filaments_toroidally(filaments):
         filaments (list of list of list of float): filaments in order of
             increasing toroidal angle.
     """
-    com_list = []
+    com_list = np.zeros((len(filaments), 3))
 
-    for fil in filaments:
-        com = np.average(fil, axis=0)
-        com_list.append(com)
+    for idx, fil in enumerate(filaments):
+        com_list[idx] = np.average(fil, axis=0)
 
-    com_list = np.array(com_list)
     phi_arr = np.arctan2(com_list[:, 1], com_list[:, 0])
 
     filaments = np.array([x for _, x in sorted(zip(phi_arr, filaments))])
@@ -89,16 +81,12 @@ def reorder_filaments(filaments):
         # start the filament at the outboard
         max_z_index = get_start_index(filament)
         reordered_filament = np.concatenate(
-            [filament[max_z_index:], filament[0:max_z_index]]
+            [filament[max_z_index:], filament[1:max_z_index]]
         )
 
         # make sure z is increasing initially
-        if filament[max_z_index, 2] > filament[max_z_index + 1, 2]:
+        if reordered_filament[0, 2] > reordered_filament[1, 2]:
             reordered_filament = np.flip(reordered_filament, axis=0)
-
-        # remove duplicate point since the start point might be different
-        _, idx = np.unique(reordered_filament, return_index=True, axis=0)
-        reordered_filament = reordered_filament[np.sort(idx)]
 
         # ensure filament is a closed loop
         reordered_filament = np.concatenate(
@@ -127,9 +115,7 @@ def get_reordered_filaments(magnet_set):
 
 
 def build_magnet_surface(reordered_filaments):
-    for fil_index, _ in enumerate(reordered_filaments[0:-1]):
-        fil1 = reordered_filaments[fil_index]
-        fil2 = reordered_filaments[fil_index + 1]
+    for fil1, fil2 in zip(reordered_filaments[0:-1], reordered_filaments[1:]):
         for index, _ in enumerate(fil1):
             x1 = fil1[index, 0]
             x2 = fil2[index, 0]
@@ -141,11 +127,11 @@ def build_magnet_surface(reordered_filaments):
                 f"create curve location {x1} {y1} {z1} location {x2} {y2} {z2}"
             )
 
-    lines = np.array(cubit.get_entities("curve"))
-    lines = np.reshape(
-        lines, (len(reordered_filaments) - 1, len(reordered_filaments[0]))
+    loops = np.array(cubit.get_entities("curve"))
+    loops = np.reshape(
+        loops, (len(reordered_filaments) - 1, len(reordered_filaments[0]))
     )
-    for loop in lines:
+    for loop in loops:
         for line in loop[0:-1]:
             cubit.cmd(f"create surface skin curve {line} {line + 1}")
 
@@ -156,14 +142,14 @@ def measure_radial_distance(ribs):
         distance_subset = []
         for point, direction in zip(rib.rib_loci, rib._normals()):
             cubit.cmd(f"create vertex {point[0]} {point[1]} {point[2]}")
-            vertex_id = max(cubit.get_entities("vertex"))
+            vertex_id = cubit.get_last_id("vertex")
             cubit.cmd(
                 f"create curve location at vertex {vertex_id} "
                 f"location fire ray location at vertex {vertex_id} "
                 f"direction {direction[0]} {direction[1]} {direction[2]} at "
                 "surface all maximum hits 1"
             )
-            curve_id = max(cubit.get_entities("curve"))
+            curve_id = cubit.get_last_id("curve")
             distance = cubit.get_curve_length(curve_id)
             distance_subset.append(distance)
         distances.append(distance_subset)
