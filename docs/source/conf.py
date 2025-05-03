037ec730061865f0b3b2d1348010c2163eff3224
Micah D. Gale <mg@gale.engineer>
2025-05-02 20:11:12 -0500

    Added API autodocing from MontePy from @tjlaboss.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,21 +7,29 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'ParaStell'
-copyright = '2025, Connor Moreno, Edgar Pflug, Paul Wilson, Eitan Weinstein, Enrique Miralles-Dolz, Joshua Smandych, Paul Romano, Bowen Quan, Jonathan Shimwell'
+copyright = '2023-2025 UW-Madison Computational Nuclear Engineering Research Group'
 author = 'Connor Moreno, Edgar Pflug, Paul Wilson, Eitan Weinstein, Enrique Miralles-Dolz, Joshua Smandych, Paul Romano, Bowen Quan, Jonathan Shimwell'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinxcontrib.apidoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 
+apidoc_module_dir = "../../parastell"
+apidoc_module_first = True
+apidoc_separate_modules = True
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
