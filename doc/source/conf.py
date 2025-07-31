9db30ec89ea5efe9ecbaffee93916317033ef466
Micah D. Gale <mg@gale.engineer>
2025-07-31 11:15:22 -0500

    Added version info to documentation site.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,6 +15,8 @@ import sys
 
 sys.path.insert(0, os.path.abspath("../.."))
 
+version = importlib.metadata.version("parastell")
+release = version
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
