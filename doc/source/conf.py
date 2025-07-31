a5c39bf743854ab3577b296f8e32ec389d3c9aa2
Micah Gale <mg@gale.engineer>
2025-07-31 14:20:24 -0500

    Made a square version of the logo.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 version = importlib.metadata.version("parastell")
 release = version
 html_logo = "parastell_logo.svg"
-favicons = ["parastell_logo.svg"]
+favicons = ["parastell_logo_sq.svg"]
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
